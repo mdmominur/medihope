8517cd7ffbbbb818af84445f50426ba5e4f54b15
mdmominur <mominur__rahman@outlook.com>
2021-10-18 20:01:58 +0600

    Complete Banner section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
+import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import bannerImage from '../../../images/Banner/bannerImage.png';
+import './Banner.css';
+const Banner = () => {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="h-100 d-flex align-items-center text-center">
+                        <div>
+                            <h1 className="display-3 text-brand fw-bold">WE CARE</h1>
+                            <h2 className="text-brand fw-normal"> ABOUT YOUR HEALTH</h2>
+                            <h2 className="text-brand fw-bold">HEALTH</h2>
+                            <p className="bg-brand-2 text-light info-top mt-3">OPEN <span className="fw-bold">24 HRS</span> </p>
+                            <p className="banner-pragraph bg-brand p-4 text-light">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born.</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="px-4 text-end">
+                    <img className="w-100" src={bannerImage} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
